44d1a37826230883413166f6fe3cb18db3bb19ec
abhaykoushik06-art <abhaykoushik06@gmail.com>
2026-08-25 16:14:30 +0530

    Add files via upload

---
+++ Project-1/sketch.ino
@@ -0,0 +1,29 @@
+#include "DHT.h"
+#define DHTPIN 4
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+void setup(){
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop(){
+  float temperature = dht.readTemperature();
+  float humidity=dht.readHumidity();
+
+    if (isnan(temperature) || isnan(humidity)) {
+    Serial.println("Failed to read from DHT sensor!");
+    delay(2000);
+    return;
+  }
+
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.print(" °C");
+
+  Serial.print(" | Humidity: ");
+  Serial.print(humidity);
+  Serial.println(" %");
+
+  delay(2000);
+}
